d4475726f45b92b177b63fbe57c749bf0ff8d909
Ryan Su <isRyanSu@gmail.com>
2024-05-29 22:43:25 +0800

    chore: build the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import background from '@/public/background.png'
+
 export default function Home() {
-  return <h1>The Wild Oasis Website</h1>
+  return (
+    <main className="mt-24">
+      <Image
+        src={background}
+        fill
+        alt="Mountains and forests with two cabins"
+        quality={80}
+        priority={true}
+        placeholder="blur" // NOTICE: 效果似乎并不明显
+        className="object-cover object-top"
+      />
+      <div className="relative z-10 text-center">
+        <h1 className="mb-10 text-8xl font-normal tracking-tight text-primary-50">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-lg font-semibold text-primary-800 transition-all hover:bg-accent-600"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
+  )
 }
