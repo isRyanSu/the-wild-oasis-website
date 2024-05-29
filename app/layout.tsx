6e961e039a2f93ac4cb4233369e19fa497a677df
Ryan Su <isRyanSu@gmail.com>
2024-05-29 20:42:35 +0800

    style(global): customize color theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { type Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import '@/styles/global.css'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} min-h-screen bg-primary-950 text-primary-100`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
